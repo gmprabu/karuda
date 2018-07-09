ed88d1132ce3ef3a55de2eff23b320fb04b8ddeb
Prabu Muniyappan <s754346@emirates.com>
2018-07-09 10:39:02 +0400

    fixed dynamic form rows issue

--- karuda-ui/src/app/product/price-panel/price-panel.component.ts
+++ karuda-ui/src/app/product/price-panel/price-panel.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../../model/product';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, FormArray } from '@angular/forms';
 import { Price } from '../../model/price';
 import { ProductService } from '../product-list/product.service';
 import { CommonService } from '../../shared/common.service';
@@ -9,15 +9,12 @@ import { CommonService } from '../../shared/common.service';
   selector: 'app-price-panel',
   templateUrl: './price-panel.component.html',
   styleUrls: ['./price-panel.component.css'],
-  providers : [ProductService],
+  providers: [ProductService],
 })
 export class PricePanelComponent implements OnInit {
 
-
- 
   @Input() product: Product;
-  form: FormGroup = this.fb.group({});
-
+  priceForm: FormGroup;
 
   unitsLiquid = [
     { name: 'Milliliter', key: 'ml' },
@@ -28,14 +25,32 @@ export class PricePanelComponent implements OnInit {
     { name: 'Kilogram', key: 'kg' }
   ];
 
-  constructor(private fb: FormBuilder,private productService : ProductService, private commonService: CommonService) { }
+  constructor(private fb: FormBuilder, private productService: ProductService, private commonService: CommonService) { }
   ngOnInit() {
-    this.product.price.forEach((item, index) => {
-      this.addFormControl(index);
+    this.priceForm = this.fb.group({
+      pricelist: this.fb.array([this.initItemRows()]) // here
     });
   }
-  getUnitType(type: String) {
 
+  initItemRows() {
+    return this.fb.group({
+      quantity: ['',Validators.required],
+      unitType: ['',Validators.required],
+      price: ['',Validators.required],
+    });
+  }
+
+  addPrice() {
+    const control = <FormArray>this.priceForm.controls['pricelist'];
+    control.push(this.initItemRows());
+  }
+
+  deletePrice(index: number) {
+    const control = <FormArray>this.priceForm.controls['pricelist'];
+    control.removeAt(index);
+  }
+
+  getUnitType(type: String) {
     if (type == "KGS") {
       return this.unitsSolid;
     } else if (type == "LTR") {
@@ -43,54 +58,24 @@ export class PricePanelComponent implements OnInit {
     }
   }
 
-  addFormControl(index: Number) {
-    this.form.addControl('quantity' + index, new FormControl('', Validators.required));
-    this.form.addControl('unitType' + index, new FormControl('', Validators.required));
-    this.form.addControl('price' + index, new FormControl('', Validators.required));
-  }
-
-  definePrice(product: Product) {
-    product.price.push(new Price());
-    this.addFormControl(this.product.price.length - 1);
-  }
-  removePrice(product: Product, index: number) {
-    if (index !== -1) {
-      product.price.splice(index, 1);
-    }
-    this.reDefineFormControl();
-  }
-
-  reDefineFormControl() {
-    this.form = this.fb.group({});
-    this.product.price.forEach((item, index) => {
-      this.addFormControl(index);
-    }); 
-    console.log(this.form);
-  }
-
-
-  getControlname(name: string, index: number): String {
-    return name + index;
-  }
-
   savePrices() {
-    if (this.form.valid) {
-      let prices:Price[]=[];
-      this.commonService.startSpinner();
-      this.product.price.forEach((item, index) => {
+    if (this.priceForm.valid) {
+      let prices: Price[] = [];
+      console.log(this.priceForm.value);
+     this.commonService.startSpinner();
+     this.priceForm.value.pricelist.forEach((item, index) => {
         let price = new Price();
-
-        price.quantity = this.form.value['quantity'+index];
-        price.unitType = this.form.value['unitType'+index];
-        price.price =  this.form.value['price'+index];
+        price.quantity = item['quantity'];
+        price.unitType = item['unitType'];
+        price.price = item['price'];
         prices.push(price);
       }); 
-      this.product.price=prices;
-      this.productService.updateProduct(this.product).subscribe(data => {
+      this.product.price = prices;
+       this.productService.updateProduct(this.product).subscribe(data => {
         this.commonService.stopSpinner();
         this.commonService.showSuccessNotification(data.message);
       });
     }
-  }
+  } 
 
 }
